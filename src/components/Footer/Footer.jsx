e067bfb250bdf61d0d3806ed40503a51cfce1c7d
swapno92 <swapnoprograming92@gmail.com>
2024-03-08 14:43:57 +0600

    updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
 // import React from 'react';
 
-const Footer = () => {
+const Footer = ({showContent}) => {
     return (
         <footer  className="footer footer-center p-4 bg-[#bd82d0] text-base-content">
             <div>
                 <p className='font-semibold text-base'>Copyright © {(new Date().getFullYear())}. Swapno Dey All Rights Reserved.</p>
             </div>
+
+            {showContent ? (
+                <p>Content is visible.</p>
+            ) : (
+                <p>Content is hidden.</p>
+            )}
+            sdf
         </footer>
     );
 };
--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,10 +1,10 @@
 // import React from 'react';
 import { FaGithub } from "react-icons/fa";
 import { TbWorldCode } from "react-icons/tb";
-import project1 from '../../assets/online-group-study.png'
-import project2 from '../../assets/automotive-brand.png'
-import project3 from '../../assets/Social-Events.png'
-import project4 from '../../assets/donation-campain.png'
+import project1 from '../../assets/Swift Mart.png'
+import project2 from '../../assets/bloodDonatin.png'
+import project3 from '../../assets/online-group-study.png'
+import project4 from '../../assets/automotive-brand.png'
 
 
 
@@ -20,10 +20,51 @@ const MyProjects = () => {
                 <p className='text-center text-[#CD5DF3] md:text-4xl text-3xl font-bold md:-mt-20 -mt-9 border-b-4 pb-3 border-[#CD5DF3] font-serif'>My Project's</p>
             </div>
             <div className=" text-white grid lg:grid-cols-2 grid-cols-1 px-8 gap-10 mt-10 ">
-                <div className=" rounded-lg space-y-5 py-4 bg-[#454242] shadow-md shadow-[#201f1f]">
+                {/* 1 */}
+                 <div className=" rounded-lg space-y-5 py-4 bg-[#454242] shadow-md shadow-[#201f1f]">
                     <div className="relative rounded-2xl w-[85%] mx-auto roun2 h-64 overflow-hidden ">
                         <div className="h-max rounded rounded-b-2xl transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)]">
                             <img src={project1} alt="" className="w-full object-cover object-top "
+                            />
+                        </div>
+                    </div>
+                    <div className='w-[80%] mx-auto flex justify-between'>
+                        <h1 className='text-2xl text-[#CD5DF3] font-bold'>Swift Mart</h1>
+                        <div className='flex gap-3'>
+                            <a target="blank" href="https://github.com/MD-TANVIR7462/Economic-project-client" ><FaGithub className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></FaGithub></a>
+                            <a href="https://ecommerce-project-7462.web.app/" target='blank'><TbWorldCode className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></TbWorldCode></a>
+                        </div>
+                    </div>
+                    <div className='w-[80%] mx-auto '>
+                        <p className="text-gray-200">Swift Mart is a Ecommerce Website where users can Buy Any Product and The admin can add or remove any product. Every user has the facility to select products according to their own preferences, product discounts can be known through the website.
+                        {/* Admin Email : testing@gmail.com || Pass: Test@T . */}
+                        </p>
+                    </div>
+                </div>
+                {/* 2 */}
+                <div className=" rounded-lg space-y-5 py-4 bg-[#454242] shadow-md shadow-[#201f1f]">
+                    <div className="relative rounded-2xl w-[85%] mx-auto roun2 h-64 overflow-hidden ">
+                        <div className="h-max rounded rounded-b-2xl transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)]">
+                            <img src={project2} alt="" className="w-full object-cover object-top "
+                            />
+                        </div>
+                    </div>
+                    <div className='w-[80%] mx-auto flex justify-between'>
+                        <h1 className='text-2xl text-[#CD5DF3] font-bold'>Drop Sync</h1>
+                        <div className='flex gap-3'>
+                            <a target="blank" href="https://github.com/swapno92/blood-donation" ><FaGithub className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></FaGithub></a>
+                            <a href="https://blood-donation-binary-avengers.vercel.app/" target='blank'><TbWorldCode className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></TbWorldCode></a>
+                        </div>
+                    </div>
+                    <div className='w-[80%] mx-auto '>
+                        <p className="text-gray-200">Drop Sync a web base blood donation system website. This website are build with next js which helped fast rendering our website.Through this wave best blood donation project, every user will be helpful in various ways. Such as emergency blood collection, timely blood donation and acquiring various knowledge related to blood donation etc.</p>
+                    </div>
+                </div>
+                {/* 3 */}
+                <div className=" rounded-lg space-y-5 py-4 bg-[#454242] shadow-md shadow-[#201f1f]">
+                    <div className="relative rounded-2xl w-[85%] mx-auto roun2 h-64 overflow-hidden ">
+                        <div className="h-max rounded rounded-b-2xl transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)]">
+                            <img src={project3} alt="" className="w-full object-cover object-top "
                             />
                         </div>
                     </div>
@@ -38,10 +79,11 @@ const MyProjects = () => {
                         <p className="text-gray-200">Online study group website is a platform for various courses and student assignments. Here every student can create assignments, read, update and delete unnecessary assignments. The examiner can mark and make comments based on the assignment</p>
                     </div>
                 </div>
+                {/* 4 */}
                 <div className=" rounded-lg space-y-5 py-4 bg-[#454242] shadow-md shadow-[#201f1f]">
                     <div className="relative rounded-2xl w-[85%] mx-auto roun2 h-64 overflow-hidden ">
                         <div className="h-max rounded rounded-b-2xl transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)]">
-                            <img src={project2} alt="" className="w-full object-cover object-top "
+                            <img src={project4} alt="" className="w-full object-cover object-top "
                             />
                         </div>
                     </div>
@@ -56,50 +98,6 @@ const MyProjects = () => {
                         <p className="text-gray-200">Automotive brand best website is created with products of different brands. There is an authentication system, there is also the facility to add available products. If the user has login, the product details can be updated and added to cart. Moreover, the website has the facility of using dark theme</p>
                     </div>
                 </div>
-                <div className=" rounded-lg space-y-5 py-4 bg-[#454242] shadow-md shadow-[#201f1f]">
-                    <div className="relative rounded-2xl w-[85%] mx-auto roun2 h-64 overflow-hidden ">
-                        <div className="h-max rounded rounded-b-2xl transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)]">
-                            <img src={project3} alt="" className="w-full object-cover object-top "
-                            />
-                        </div>
-                    </div>
-                    <div className='w-[80%] mx-auto flex justify-between'>
-                        <h1 className='text-2xl text-[#CD5DF3] font-bold'>Social Events</h1>
-                        <div className='flex gap-3'>
-                            <a target="blank" href="https://github.com/swapno92/Social-Events" ><FaGithub className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></FaGithub></a>
-                            <a href="https://assignment-nine-2294f.web.app/" target='blank'><TbWorldCode className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></TbWorldCode></a>
-                        </div>
-                    </div>
-                    <div className='w-[80%] mx-auto '>
-                        <p className="text-gray-200">By visiting the social event website, you can know about all the services and details of the event. Here are the prices and complete details of various event services. Plus a list of all our clients and feedback. The photos page features some photos from our various events.</p>
-                    </div>
-                </div>
-                <div className=" rounded-lg space-y-5 py-4 bg-[#454242] shadow-md shadow-[#201f1f]">
-                    <div className="relative rounded-2xl w-[85%] mx-auto roun2 h-64 overflow-hidden ">
-                        <div className="h-max rounded rounded-b-2xl transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)]">
-                            <img src={project4} alt="" className="w-full object-cover object-top "
-                            />
-                        </div>
-                    </div>
-                    <div className='w-[80%] mx-auto flex justify-between'>
-                        <h1 className='text-2xl text-[#CD5DF3] font-bold'>Donation Campain</h1>
-                        <div className='flex gap-3'>
-                            <a target="blank" href="https://github.com/swapno92/Donation-Campain" ><FaGithub className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></FaGithub></a>
-                            <a href="https://courageous-meringue-627b87.netlify.app/" target='blank'><TbWorldCode className='text-3xl  hover:text-[#77d7f2]  border-[#d23434] rounded-full '></TbWorldCode></a>
-                        </div>
-                    </div>
-                    <div className='w-[80%] mx-auto '>
-                        <p className="text-gray-200">Created a fully functional e-commerce website called TrendHaven to provide users with a seamless online shopping experience, backend repo: https://github.com/AJAmran/mern-e-commerce-server.git</p>
-                    </div>
-                </div>
-                {/* <div className='md:w-[30%]'> */}
-                    {/* <div className="relative max-w-full h-72 overflow-hidden rounded-md">
-                        <div className="h-max w-full transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)]">
-                            <img src={project2} alt="" className="w-full object-cover object-top"
-                            />
-                        </div>
-                    </div> */}
-                {/* </div> */}
             </div>
         </div>
     );
